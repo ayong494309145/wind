d5d46fe0c1721e1370e541a6ffaf579890c2323a
邱勇 <494309145@qq.com>
2016-11-16 12:12:58 +0800

    按时按时

---
+++ app/src/main/java/com/example/administrator/wind/HelloActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.wind;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/11/16.
+ */
+public class HelloActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
